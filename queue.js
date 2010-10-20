c1ba9d4a9b8422f1b761d900c90cedc59f4fa731
Paul Spencer <paul@pagameba.com>
2010-10-20 12:14:53 -0400

    adding web server to queue

--- queue.js
+++ queue.js
@@ -1,6 +1,7 @@
 var net = require('net'),
     http = require('http'),
     sys = require("sys"),
+    url = require('url'),
     microseconds = require("./deps/node-microseconds/lib/node-microseconds"),
     EventEmitter = require('events').EventEmitter,
     mongo = require('./deps/node-mongodb-native/lib/mongodb'),
@@ -13,7 +14,8 @@ var net = require('net'),
     messages = [],
     waiters = [],
     emitter = new EventEmitter(),
-    host = 'ec2-174-129-57-251.compute-1.amazonaws.com'; //'127.0.0.1',
+    host = 'localhost',
+    web_port = 8080,
     push_port = 8081,
     pull_port = 8082,
     alive_port = 8083;
@@ -58,8 +60,7 @@ db.open(function(err,db) {
         if (messages.length) {
           message = messages.shift();
           collection.remove({_id: message._id}, function(err, collection) {});
-          stream.write(message.message);
-          stream.end();
+          stream.end('{"success":true,message:'+message+'}');
           sys.puts('Dequeued message ' + message._id + ' at timestamp ' + message.timestamp);
         } else {
           waiters.push(stream);
@@ -70,40 +71,85 @@ db.open(function(err,db) {
     });
     pull.listen(pull_port, host);
     
+    var web = http.createServer(function(request,response){
+      var parsed = url.parse(request.url, true),
+          output = {
+            success: false,
+            message: 'Unknown error'
+          },
+          message;
+      response.writeHead(200, {
+        'Content-type':'text/json'
+      });
+      switch(parsed.pathname) {
+        case '/push':
+          if (parsed['query'] && parsed.query['message']) {
+            sys.puts('got message via http: '+parsed.query.message);
+            var ts = microseconds.milliseconds();
+            collection.insert({message:parsed.query.message, timestamp: ts},function(err, docs) {
+              if (err) {
+                sys.puts(sys.inspect(err));
+                output.success = false;
+                output.message = "failed to insert message.";
+                output.detail = err;
+                response.end(sys.inspect(output));
+              } else {
+                for (var i=0; i<docs.length; i++) {
+                  messages.push(docs[i]);
+                  sys.puts('Enqueued message ' + docs[i].id + ' at timestamp ' + docs[i].timestamp);
+                }
+                emitter.emit('message');
+                output.success = true;
+                output.message = "message enqueueued";
+                output.id = ''+docs[0]._id;
+                response.end(sys.inspect(output));
+              }
+            });
+          } else {
+            output.message = 'push requires message parameter';
+            response.end(sys.inspect(output));
+          }
+          break;
+        case '/pull':
+          if (messages.length) {
+            message = messages.shift();
+            collection.remove({_id: message._id}, function(err, collection) {});
+            output.success = true;
+            output.message = message;
+            response.end(sys.inspect(output));
+            sys.puts('Dequeued message ' + message._id + ' at timestamp ' + message.timestamp);
+          } else {
+            waiters.push(response);
+          }
+          break;
+        case '/list':
+          output.success = true;
+          output.message = messages;
+          response.end(sys.inspect(output));
+          break;
+        default:
+          output.message = "Invalid request path "+request.pathname+", should be /push or /pull";
+          response.end(sys.inspect(output));
+      }
+    }).listen(web_port);
+    console.log('Web Server running on port ' + web_port);
+
+    
     emitter.on('message', function(){
+      var output = {
+        success: true,
+        message: ''
+      };
       if (messages.length && waiters.length) {
         stream = waiters.shift();
         message = messages.shift();
         collection.remove({_id: message._id}, function(err, collection) {});
-        stream.write(message.message);
-        stream.end();
+        output.message = message;
+        stream.end(sys.inspect(output));
         sys.puts('Dequeued message ' + message._id + ' at timestamp ' + message.timestamp);
       }
     });
   });
 });
 
-/*
-http.createServer(function(request,response){
-  response.writeHead(200, {
-    'Content-type':'text/plain'
-  });
-  response.end('Hello World\n');
-}).listen(8124);
-console.log('Server running at http://localhost:8124/');
 
-var net = require('net');
-var server = net.createServer(function(stream) {
-  stream.setEncoding('utf8');
-  stream.on('connect', function(){
-    stream.write('hello\r\n');
-  });
-  stream.on('data', function(data) {
-    stream.write(data);
-  });
-  stream.on('end', function() {
-    stream.write('goodbye\r\n');
-  });
-});
-server.listen(8125,'localhost');
-*/
